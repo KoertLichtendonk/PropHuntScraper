34d19d120e4f43e9de759a33ddbc1d83142965d5
Koert Lichtendonk <koert@adjust-it.nl>
2021-07-09 09:05:35 +0200

    upd

--- PropHuntScraper/Program.cs
+++ PropHuntScraper/Program.cs
@@ -1,6 +1,10 @@
 ﻿using OpenQA.Selenium.Firefox;
+using Redmine.Net.Api;
+using Redmine.Net.Api.Async;
+using Redmine.Net.Api.Types;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Timers;
 
@@ -8,16 +12,25 @@ namespace PropHuntScraper
 {
     class Program
     {
+        public static RedmineManager _rm;
+
         static void Main(string[] args)
         {
             Console.WriteLine("PropHuntScraper is being configured.");
 
-            StartScraping(null, null);
-
+            // Timer
             Timer Timer = new Timer(3600000);
             Timer.Elapsed += StartScraping;
             Timer.AutoReset = true;
             Timer.Enabled = true;
+
+            // Redmine API
+            _rm = new RedmineManager("https://redmine.koertlichtendonk.nl/", "f35cb859d29933894c7bd3e7ff006d262d9e420b", MimeFormat.Json, false);
+
+            // Force first scraping process
+            Console.WriteLine("PropHuntScraper is starting first scraping process.");
+            StartScraping(null, null);
+            Console.WriteLine("PropHuntScraper is ending the first scraping process.");
         }
 
         private static void StartScraping(object sender, ElapsedEventArgs e)
@@ -37,6 +50,8 @@ namespace PropHuntScraper
             FirefoxDriver Driver = new FirefoxDriver(service, options);
             Driver.Manage().Timeouts().PageLoad = TimeSpan.FromHours(1);
 
+            Console.WriteLine("Navigate to first page to retreive amount of pages.");
+
             Driver.Navigate().GoToUrl("https://prophunt.biz/unowned?page=1");
 
             string Body = Driver.GetTrimmedBody();
@@ -53,10 +68,14 @@ namespace PropHuntScraper
                 }
             }
 
+            Console.WriteLine("There are a total of "+ pages +" pages.");
+
             List<HouseInfo> unownedHouses = new List<HouseInfo>();
 
             for (int i = 1; i < pages; i++)
             {
+                Console.WriteLine("Starting to retreive houses from page " + i + ".");
+
                 Driver.Navigate().GoToUrl("https://prophunt.biz/unowned?page=" + i);
 
                 string pageBody = Driver.GetTrimmedBody();
@@ -72,7 +91,49 @@ namespace PropHuntScraper
                     house.price = match.Groups[3].Value.Trim();
                     house.custom_interior = match.Groups[4].Value.Trim();
 
+                    Console.WriteLine(house.address + " was found for sale costing " + house.price + ".");
                     unownedHouses.Add(house);
+                }
+
+                Console.WriteLine("Finished to retreive houses from page " + i + ".");
+            }
+
+            Console.WriteLine("Begin to get all projects by identifier.");
+            var AllProjects = RedmineHelper.GetAllProjectsByIdentifier();
+            Console.WriteLine("End of getting all projects by identifier.");
+
+            if (AllProjects.TryGetValue("unowned-houses", out Project project) == false)
+            {
+                Console.WriteLine("Can't find the Unowned Houses project in Redmine.");
+                return;
+            }
+
+            Console.WriteLine("Begin to get all issues from project.");
+            var AllIssuesFromProject = RedmineHelper.GetAllIssuesFromProject(true, project.Id.ToString());
+            Console.WriteLine("End of getting all issues from project.");
+
+            foreach (HouseInfo house in unownedHouses)
+            {
+                if(!AllIssuesFromProject.Keys.Any(i => i.Contains(house.address)))
+                {
+                    Issue new_issue = new Issue();
+
+                    new_issue.Project = IdentifiableName.Create<Project>(project.Id);
+                    new_issue.Subject = house.address;
+                    RedmineHelper.GetAllTrackers().TryGetValue("Todo", out Tracker tracker);
+                    new_issue.Status = IdentifiableName.Create<Tracker>(tracker.Id);
+                    new_issue.AssignedTo = IdentifiableName.Create<IdentifiableName>( RedmineHelper.GetAllUsers().First().Value.Id );
+                    RedmineHelper.GetAllStatuses().TryGetValue("New", out IssueStatus issueStatus);
+                    new_issue.Tracker = IdentifiableName.Create<IssueStatus>(issueStatus.Id);
+                    new_issue.Description = "!"+house.image+"!\n\n*Price*: "+house.price+"\n*Custom Interior*: "+house.custom_interior;
+                    new_issue.StartDate = DateTime.Now;
+
+                    Console.WriteLine("Sending house "+ house.address +" to Redmine.");
+                    Program._rm.CreateObjectAsync(new_issue);
+                    Console.WriteLine("Sent house " + house.address + " to Redmine.");
+                } else
+                {
+                    Console.WriteLine("House " + house.address + " is already on Redmine.");
                 }
             }
         }
---
+++ PropHuntScraper/RedmineHelper.cs
@@ -0,0 +1,89 @@
+﻿using Redmine.Net.Api.Types;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace PropHuntScraper
+{
+    public class RedmineHelper
+    {
+        private static Dictionary<string, Project> _AllProjects { get; set; } = null;
+        public static Dictionary<string, Project> GetAllProjectsByIdentifier(bool force = false)
+        {
+            if (_AllProjects == null || force == true)
+            {
+                _AllProjects = new Dictionary<string, Project>();
+
+                NameValueCollection parameters = new NameValueCollection { }; // *
+                foreach (var Project in Program._rm.GetObjects<Project>(parameters))
+                {
+                    _AllProjects.Add(Project.Identifier, Project);
+                }
+            }
+            return _AllProjects;
+        }
+
+        private static Dictionary<string, Issue> _AllIssuesFromProject { get; set; } = null;
+        public static Dictionary<string, Issue> GetAllIssuesFromProject(bool force = false, string project_id = "*")
+        {
+            if (_AllIssuesFromProject == null || force == true)
+            {
+                _AllIssuesFromProject = new Dictionary<string, Issue>();
+
+                NameValueCollection parameters = new NameValueCollection { { "project_id", project_id }, { "status_id", "*" } };
+                foreach (var Issue in Program._rm.GetObjects<Issue>(parameters))
+                {
+                    _AllIssuesFromProject.Add(String.Format("#{0}: {1}", Issue.Id, Issue.Subject), Issue);
+                }
+            }
+            return _AllIssuesFromProject;
+        }
+
+        private static Dictionary<string, IssueStatus> _AllStatuses { get; set; } = null;
+        public static Dictionary<string, IssueStatus> GetAllStatuses()
+        {
+            if (_AllStatuses == null)
+            {
+                _AllStatuses = new Dictionary<string, IssueStatus>();
+
+                foreach (var Status in Program._rm.GetObjects<IssueStatus>())
+                {
+                    _AllStatuses.Add(String.Format("{0}", Status.Name), Status);
+                }
+            }
+            return _AllStatuses;
+        }
+
+        private static Dictionary<string, Tracker> _AllTrackers { get; set; } = null;
+        public static Dictionary<string, Tracker> GetAllTrackers()
+        {
+            if (_AllTrackers == null)
+            {
+                _AllTrackers = new Dictionary<string, Tracker>();
+
+                foreach (var Tracker in Program._rm.GetObjects<Tracker>())
+                {
+                    _AllTrackers.Add(String.Format("{0}", Tracker.Name), Tracker);
+                }
+            }
+            return _AllTrackers;
+        }
+
+        private static Dictionary<string, User> _AllUsers { get; set; } = null;
+        public static Dictionary<string, User> GetAllUsers(bool force = false)
+        {
+
+            if (_AllUsers == null || force == true)
+            {
+                _AllUsers = new Dictionary<string, User>();
+
+                foreach (var User in Program._rm.GetObjects<User>())
+                {
+                    _AllUsers.Add(String.Format("{0} {1}", User.FirstName, User.LastName), User);
+                }
+            }
+            return _AllUsers;
+        }
+    }
+}
